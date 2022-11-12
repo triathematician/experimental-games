ad66f90865a9759010260bf69ea4aaad69059de2
Elisha Peterson <triathematician@gmail.com>
2022-11-12 11:11:37 -0500

    license and copyrights

--- greek-clash-td/src/main/kotlin/asher/greek/util/Geometry.kt
+++ greek-clash-td/src/main/kotlin/asher/greek/util/Geometry.kt
@@ -120,7 +120,27 @@ operator fun Node.contains(p: Point2) = contains(javafx.geometry.Point2D(p.x, p.
 
 var Circle.center: Point2
     get() = Point2(centerX, centerY)
-    set(value) { centerX = value.x; centerY = value.y }
+    set(value) { centerX = value.x;
+/*-
+ * #%L
+ * greek-clash-td
+ * --
+ * Copyright (C) 2020 - 2022 Elisha Peterson and Asher Peterson
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+ centerY = value.y }
 
 var Text.anchor: Point2
     get() = Point2(x, y)
